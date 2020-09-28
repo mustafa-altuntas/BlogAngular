c3b1f2f68db5f74fea5725ab4533b496c9569dff
Mustafa ALTUNTAŞ <altun_tas@outlook.com>
2020-09-28 13:36:47 +0300

    Makale fotoğrafları için file upload tamamlandı.

--- src/app/admin-pages/article/article-add/article-add.component.ts
+++ src/app/admin-pages/article/article-add/article-add.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { ArticleService } from 'src/app/services/article.service';
 
 @Component({
   selector: 'app-article-add',
   templateUrl: './article-add.component.html',
-  styleUrls: ['./article-add.component.css']
+  styleUrls: ['./article-add.component.css'],
 })
 export class ArticleAddComponent implements OnInit {
+  fileData: File = null;
+  picture: string = null;
 
-  constructor() { }
+  constructor(private articleService: ArticleService) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  upload(files) {
+    //eğer kullanıcı 1 den fazla dosya seçerse yanlızca ilk seçtiği dosyayı alıcağız
+    this.fileData = files.target.files[0];
+
+    let formData = new FormData();
+    formData.append('picture', this.fileData);
+
+    this.articleService.saveArticlePicture(formData).subscribe((result) => {
+      this.picture = result.path;
+    });
   }
-
 }
--- src/app/services/article.service.ts
+++ src/app/services/article.service.ts
@@ -1,8 +1,8 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
 import { ArticlePg } from '../models/article-pg';
-import { tap } from 'rxjs/operators';
+import { tap, catchError } from 'rxjs/operators';
 import { Article } from '../models/article';
 import { Archive } from '../models/archive';
 
@@ -14,11 +14,9 @@ export class ArticleService {
   public loading: boolean = true;
   private apiUrl: string = 'https://localhost:44384/api/articles';
 
-
-  getArticlesWithoutPg():Observable<Article[]>{
-    return this.httpClient.get<Article[]>(this.apiUrl)
+  getArticlesWithoutPg(): Observable<Article[]> {
+    return this.httpClient.get<Article[]>(this.apiUrl);
   }
-
 
   public getAeticles(page: number, pageSize: number): Observable<ArticlePg> {
     let url = `${this.apiUrl}/${page}/${pageSize}`;
@@ -86,18 +84,29 @@ export class ArticleService {
     );
   }
 
-
-  articleViewCountUp(id:number){
+  articleViewCountUp(id: number) {
     let url = `${this.apiUrl}/ArticleViewCountUp/${id}`;
     return this.httpClient.get(url);
-
   }
 
+  saveArticlePicture(image): Observable<any> {
+    let url = `${this.apiUrl}/SaveArticlePicture`;
 
+    return this.httpClient.post<any>(url, image);
+  }
 
-
-
-
-
-
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong.
+      console.error(
+        `Backend returned code ${error.status}, ` + `body was: ${error.error}`
+      );
+    }
+    // Return an observable with a user-facing error message.
+    return throwError('Something bad happened; please try again later.');
+  }
 }
