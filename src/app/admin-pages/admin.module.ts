e2e894f56024420a8308c2bab405c028dddbb6da
Mustafa ALTUNTAŞ <altun_tas@outlook.com>
2020-09-29 16:57:57 +0300

    CKEditor 5 module admin module eklendi

--- src/app/admin-pages/admin.module.ts
+++ src/app/admin-pages/admin.module.ts
@@ -1,9 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AppRoutingModule } from "../app-routing.module";
-import { MaterialModule } from "../modules/material//material.module";
-import { ComponentsModule } from "../components/components.module";
-
+import { AppRoutingModule } from '../app-routing.module';
+import { MaterialModule } from '../modules/material//material.module';
+import { ComponentsModule } from '../components/components.module';
 
 import { AdminLayoutComponent } from '../layout/admin-layout/admin-layout.component';
 import { AdminNavComponent } from '../nav/admin-nav/admin-nav.component';
@@ -13,12 +12,24 @@ import { ArticleAddComponent } from './article/article-add/article-add.component
 import { ArticleUpdateComponent } from './article/article-update/article-update.component';
 import { ArticleListComponent } from './article/article-list/article-list.component';
 
-
+import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
 
 @NgModule({
-  declarations: [AdminLayoutComponent,AdminNavComponent, AdminHomeComponent, AdminArticleComponent, ArticleAddComponent, ArticleUpdateComponent, ArticleListComponent],
+  declarations: [
+    AdminLayoutComponent,
+    AdminNavComponent,
+    AdminHomeComponent,
+    AdminArticleComponent,
+    ArticleAddComponent,
+    ArticleUpdateComponent,
+    ArticleListComponent,
+  ],
   imports: [
-    CommonModule,AppRoutingModule,MaterialModule,ComponentsModule
-  ]
+    CommonModule,
+    AppRoutingModule,
+    MaterialModule,
+    ComponentsModule,
+    CKEditorModule,
+  ],
 })
-export class AdminModule { }
+export class AdminModule {}
